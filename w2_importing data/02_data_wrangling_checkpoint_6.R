deae140291e60ca3de1f6f689340999d06da727a
unknown <m0988463069@gmail.com>
2019-05-20 11:44:51 +0800

    first

--- w2_importing data/02_data_wrangling_checkpoint_6.R
+++ w2_importing data/02_data_wrangling_checkpoint_6.R
@@ -1,13 +1,13 @@
 # DS4B 101-R: R FOR BUSINESS ANALYSIS ----
 # DATA WRANGLING OVERVIEW ----
-rm(list=ls())
+
 
 library(tidyverse)
 library(readxl)
 
-bikes_tbl           <- read_excel("bikes.xlsx")
-orderlines_tbl      <- read_excel("orderlines.xlsx")
-bike_orderlines_tbl <- read_rds("data_wrangled_student/bike_orderlines.rds")
+bikes_tbl           <- read_excel("00_data/bike_sales/data_raw/bikes.xlsx")
+orderlines_tbl      <- read_excel("00_data/bike_sales/data_raw/orderlines.xlsx")
+bike_orderlines_tbl <- read_rds("00_data/bike_sales/data_wrangled/bike_orderlines.rds")
 
 
 bikes_tbl
